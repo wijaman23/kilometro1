b163bc19f4a351e759697e77a67e6f710c0b27f5
wijaman23 <wijaman23@gmail.com>
2025-12-15 20:25:54 +0100

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://kilometro1-backend.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
