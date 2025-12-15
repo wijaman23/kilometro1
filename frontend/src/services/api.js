26d3c5c5b7dc608e43fc6f5b5b349902b21e0ccf
wijaman23 <wijaman23@gmail.com>
2025-12-15 20:31:05 +0100

    Fix API baseURL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kilometro1-backend.onrender.com",
+  baseURL: "https://kilometro1-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
