2a4527b265c91fde923c45dfda4b0c5c1194c778
wijaman23 <wijaman23@gmail.com>
2025-12-19 18:51:02 +0100

    Corregir front2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kilometro1-backend.onrender.com/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://kilometro1-backend.onrender.com/api",
 });
 
 /**
