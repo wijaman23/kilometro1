5f0fe5baa91c77d366a2eb6eb848ad60a41069ca
wijaman23 <wijaman23@gmail.com>
2026-05-07 18:56:29 +0200

    Linea css y botones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,32 @@
 import { Link } from 'react-router-dom'
-import trainer from '../assets/trainer.png'
+
 import logo from '../assets/LOGO_2_BLANCO.svg'
 
 function Home() {
   return (
     <main className="home-page">
-      <nav className="top-nav">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="Kilómetro 1" className="logo-img" />
+      <div className="hero-background"></div>
+
+      <nav className="hero-navbar">
+        <Link to="/" className="hero-logo-link">
+          <img src={logo} alt="Kilómetro 1" className="hero-logo-img" />
         </Link>
 
-        <Link to="/videos" className="top-video-btn">
+        <Link to="/videos" className="hero-videos-btn">
           ▶ Ver vídeos
         </Link>
       </nav>
 
-      <section className="hero">
-        <img src={trainer} alt="Entrenadores Kilómetro 1" className="hero-img" />
+      <section className="hero-content">
+        <div className="hero-text">
+          <p className="hero-subtitle">Comunidad runner</p>
 
-        <div className="hero-overlay"></div>
-
-        <div className="hero-content">
-          <p className="eyebrow">Comunidad runner</p>
-
-          <h1>
-            Somos <br />
-            <span>Kilometro1</span>
+          <h1 className="hero-title">
+            <span>Somos</span>
+            <span>Kilómetro1</span>
           </h1>
 
-          <p className="hero-text">
+          <p className="hero-description">
             Corredores de todo el mundo persiguiendo la{' '}
             <strong>EXCELENCIA</strong> también en el <strong>RUNNING</strong>.
           </p>
@@ -38,10 +36,14 @@ function Home() {
               href="https://www.instagram.com/equipokm1/?hl=es"
               target="_blank"
               rel="noreferrer"
-              className="btn-primary-km"
+              className="hero-community-btn"
             >
               Únete a la comunidad
             </a>
+
+            <Link to="/videos" className="hero-secondary-btn">
+              Ver vídeos
+            </Link>
           </div>
         </div>
       </section>
